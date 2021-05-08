0b0599d0c3c1c21aa6fc434b8c9e534e8a8de957
devsince2021 <dev.ryu2021@gmail.com>
2021-05-08 09:27:12 +0900

    add proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,15 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const apiTarget = 'http://api.travelparking.online';
+
+
+module.exports = function (app) {
+
+  app.use(
+    '/',
+    createProxyMiddleware({
+      target: apiTarget,
+      changeOrigin: true,
+    }),
+  );
+};
